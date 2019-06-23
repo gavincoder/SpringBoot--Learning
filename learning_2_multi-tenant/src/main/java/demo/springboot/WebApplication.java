92c3093933d7cfba251c9fc2e013e67625cea3ff
xsh003 <xsh003@126.com>
2019-06-23 15:03:16 +0800

    SAAS多租户技术实现

--- learning_2_multi-tenant/src/main/java/demo/springboot/WebApplication.java
+++ learning_2_multi-tenant/src/main/java/demo/springboot/WebApplication.java
@@ -10,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WebApplication {
     public static void main(String[] args) {
+		
         SpringApplication.run(WebApplication.class, args);
     }
 }
